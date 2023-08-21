a4a498458e292af6658c4bfca25536e2f65997ea
Казакова Анастасия <86001142+NastyAnanasty@users.noreply.github.com>
2023-08-21 11:40:49 +0400

    Update project4.sql

--- project_1.4/project4.sql
+++ project_1.4/project4.sql
@@ -7,20 +7,20 @@ SELECT *  FROM DDS.ft_posting_f;
 CREATE OR REPLACE FUNCTION min_max_info(input_date DATE)
 RETURNS TABLE (
     transaction_date DATE,
-    min_credit_amount NUMERIC,
-    max_credit_amount NUMERIC,
-    min_debit_amount NUMERIC,
-    max_debit_amount NUMERIC
+    min_credit_amount REAL,
+    max_credit_amount REAL,
+    min_debit_amount REAL,
+    max_debit_amount REAL
 )
 LANGUAGE plpgsql AS $func$
 BEGIN
     RETURN QUERY
     SELECT
         input_date,
-        MIN(CASE WHEN credit_amount IS NOT NULL THEN credit_amount ELSE 0 END)::NUMERIC AS min_credit_amount,
-        MAX(CASE WHEN credit_amount IS NOT NULL THEN credit_amount ELSE 0 END)::NUMERIC AS max_credit_amount,
-        MIN(CASE WHEN debet_amount IS NOT NULL THEN debet_amount ELSE 0 END)::NUMERIC AS min_debit_amount,
-        MAX(CASE WHEN debet_amount IS NOT NULL THEN debet_amount ELSE 0 END)::NUMERIC AS max_debit_amount
+        MIN(CASE WHEN credit_amount IS NOT NULL THEN credit_amount ELSE 0 END) AS min_credit_amount,
+        MAX(CASE WHEN credit_amount IS NOT NULL THEN credit_amount ELSE 0 END) AS max_credit_amount,
+        MIN(CASE WHEN debet_amount IS NOT NULL THEN debet_amount ELSE 0 END) AS min_debit_amount,
+        MAX(CASE WHEN debet_amount IS NOT NULL THEN debet_amount ELSE 0 END) AS max_debit_amount
     FROM DDS.ft_posting_f
     WHERE TO_CHAR(oper_date, 'YYYY-MM-DD') = TO_CHAR(input_date, 'YYYY-MM-DD');
 END
